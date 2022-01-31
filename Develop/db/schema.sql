c2c26c81ce08979141377f8aa43b672e5e6be9af
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-30 22:33:51 -0500

    created catefory & profuct tables

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,23 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- DATABASE TABLES
+CREATE TABLE category (
+    category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(30) NOT NULL,
+)  
+
+CREATE TABLE product (
+    product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30),
+    price DECIMAL (5,2),
+    stock INT NOT NULL VALUES(DEFAULT(10)),
+    category_id INT, 
+    FOREIGN KEY (category)
+    REFERENCES category (category_id) 
+    ON DELETE CASCADE
+)
+
+CREATE TABLE tag (
+    tag_id
+)
